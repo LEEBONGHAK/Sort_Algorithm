c31c4ea9376415295fd40dbc1f86d9edb65e007c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-10-05 16:38:08 +0900

    Bucket Sort

---
+++ bucket_sort/README.md
@@ -0,0 +1,22 @@
+# Bucket Sort  
+  
+ - 가장 간단한 정렬 알고리즘  
+ - 처리가 간단한 대신 장소적 자원(배열의 크기)을 많이 소모함  
+ - 빈 양동이 배열을 준비하고, 양동이의 번호와 일치하는 모든 데이터를 저장한 다 1번째 양동이의 데이터부터 꺼내는 것  
+  
+## Time Complexity	 
+  
+|Situation|Big O|
+|:---:|:---:|
+|Best|O(n+k)|
+|Worst|O(n2)|
+|Average|O(n)|
+|Space Complexity|O(n+k)|
+|Stability|Yes|
+  
+## 참고  
+  
+ - 스기우라 켄 / 그림으로 배우는 알고리즘 Algorithm Basic
+ - jooharm's blog / https://blog.naver.com/jooharm/221600060686
+ - GeeksforGeeks / https://www.geeksforgeeks.org/bucket-sort-2/
+ - ratsgo's blog / https://ratsgo.github.io/data%20structure&algorithm/2017/10/18/bucketsort/
--- bucket_sort/bucket_sort.c
+++ bucket_sort/bucket_sort.c
@@ -2,28 +2,7 @@
 
 #define size_of_array 100
 
-/*
-	Bucket Sort
-
-	- 가장 간단한 정렬 알고리즘
-	- 처리가 간단한 대신 장소적 자원(배열의 크기)을 많이 소모함
-	- 빈 양동이 배열을 준비하고, 양동이의 번호와 일치하는 모든 데이터를 저장한
-	  다음에 1번째 양동이의 데이터부터 꺼내는 것
-
-	Time Complexity	 
-	Best	O(n+k)
-	Worst	O(n2)
-	Average	O(n)
-	Space Complexity	O(n+k)
-	Stability	Yes
-
-
-	- 참고
-	스기우라 켄 / 그림으로 배우는 알고리즘 Algorithm Basic
-	jooharm's blog / https://blog.naver.com/jooharm/221600060686
-	GeeksforGeeks / https://www.geeksforgeeks.org/bucket-sort-2/
-	ratsgo's blog / https://ratsgo.github.io/data%20structure&algorithm/2017/10/18/bucketsort/
-*/
+/* Bucket Sort */
 
 // 0.0 ~ 10.0 사이의 소수점 첫번째 자리를 가지는 숫자로 이루어진 array를 정렬하는 bucket sort
 void Bucket_Sort(double arr[], int n) {
